6c038278e43d5e36e4c269624adfcc8ba22d28b7
SilasFelipeDev <devsilasfelipe@gmail.com>
2024-11-03 14:08:03 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function Home(){
+    return (
+        <div>
+            <h1>Para Juliane</h1>
+            <p>Que as dificuldades da vida nunca sejam 
+                maiores que a força que nos move: o AMOR
+            </p>
+            <h2>Sempre juntos</h2>
+        </div>
+    );
+}
+
+
+export default Home;
