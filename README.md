1c7450a95fd70f7885bf22f01ef9d771073b74ca
SilasFelipeDev <devsilasfelipe@gmail.com>
2024-11-10 00:23:29 +0000

    Adiciona os scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # altnews
-Repositório usado para as aulas do curso.dev - clone do projeto https://www.tabnews.com.br/  
+
+Repositório usado para as aulas do curso.dev - clone do projeto https://www.tabnews.com.br/
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
-function Home(){
-    return (
-        <div>
-            <h1>Memento Mori</h1>
-            <p>Lembre-se que você é mortal. Viva seus 
-                sonhos. Temos tão pouco tempo.
-            </p>        
-        </div>
-    );
+function Home() {
+  return (
+    <div>
+      <h1>Memento Mori</h1>
+      <p>
+        Lembre-se que você é mortal. Viva seus sonhos. Temos tão pouco tempo.
+      </p>
+    </div>
+  );
 }
-
 
 export default Home;
